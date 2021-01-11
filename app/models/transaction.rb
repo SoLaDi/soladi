9859d18b590a3b27f6fae5c54fd5721746914757
Marco Hoyer <marco.hoyer@solarisbank.de>
2021-01-11 17:41:54 +0100

    working draft of csv import validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,24 +12,64 @@
 #  updated_at  :datetime         not null
 #
 class Transaction < ApplicationRecord
-    require 'csv'
-    
-    def self.import(file)
-        puts file
-        CSV.foreach(file.path, headers:false, col_sep:";") do |row|
+  validates :entry_date, presence: true
+  validates :sender, presence: true
+  validates :description, presence: true
+  validates :amount, presence: true
+  validates :currency, presence: true
 
-            unless row.at(0).nil? || row.at(0) == "Buchung"
-                puts row
-                amount = row.at(8).split().at(1)
+  require 'csv'
 
-                newTransaction = Transaction.create(
-                    entry_date: row.at(0),
-                    sender: row.at(4),
-                    description: row.at(7),
-                    amount: amount,
-                    currency: row.at(9)
-                )
+  def self.import(file)
+    total_rows_count = 0
+    imported_rows = []
+    duplicate_rows = []
+    invalid_rows = []
+
+    CSV.foreach(file.path, headers: false, col_sep: ";") do |row|
+      total_rows_count += 1
+
+      # filter header lines
+      if row.at(0).nil? || row.at(0) == "Buchung"
+        invalid_rows.push row
+      else
+
+        # filter lines with empty amounts
+        if row.at(8).nil?
+          invalid_rows.push row
+        else
+          amount, type = row.at(8).strip.split
+
+          # we're only interested in incoming money, thus
+          # filter for incoming transactions (S == soll == outgoing / H == haben == incoming)
+          if type == "H"
+            begin
+              if Transaction.new(
+                entry_date: row.at(0),
+                sender: row.at(4),
+                description: row.at(7),
+                amount: amount,
+                currency: row.at(9)
+              ).save
+                imported_rows.push row
+              else
+                invalid_rows.push row
+              end
+
+            rescue ActiveRecord::RecordNotUnique
+              duplicate_rows.push row
+              puts "Record already exists"
             end
+          else
+            invalid_rows.push row
+          end
         end
+      end
     end
+
+    puts "total rows: #{total_rows_count}"
+    puts "imported rows: #{imported_rows.length}"
+    puts "duplicate rows: #{duplicate_rows.length}"
+    puts "invalid rows: #{invalid_rows.length}"
+  end
 end

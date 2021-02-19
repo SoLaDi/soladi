92187cb23eafce03cd8de12bbc6b5b147edcba94
Marco Hoyer <marco.hoyer@solarisbank.de>
2021-02-19 10:13:31 +0100

    fix transaction edit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_transaction, only: [:show, :update, :destroy]
+  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
 
   # GET /transactions
   # GET /transactions.json

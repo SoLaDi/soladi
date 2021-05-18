fed94e25bda638814f7dc3243258de02e58b1a91
Marco Hoyer <marco.hoyer@solarisbank.de>
2021-05-18 23:21:27 +0200

    add a form to create transactions manually

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -82,13 +82,14 @@ class TransactionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_transaction
-      @transaction = Transaction.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def transaction_params
-      params.require(:transaction).permit(:membership_id, :status_message, :status)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def transaction_params
+    params.require(:transaction).permit(:membership_id, :status_message, :status, :entry_date, :amount, :currency, :sender, :description)
+  end
 end

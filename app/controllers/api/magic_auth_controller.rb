36be769f682f5f75503ec5486ff5f08f9c778c6e
marco-hoyer <3943275+marco-hoyer@users.noreply.github.com>
2022-03-02 23:37:55 +0100

    expose a persons email address on magic login response

--- app/controllers/api/magic_auth_controller.rb
+++ app/controllers/api/magic_auth_controller.rb
@@ -7,7 +7,7 @@ class Api::MagicAuthController < Api::ApiController
       "surname": @person.surname,
       "membership": {
         "id": @person.membership_id,
-        "users": @person.membership.people.map { |p| p.as_json(only: %w[id name surname]) },
+        "users": @person.membership.people.map { |p| p.as_json(only: %w[id name surname email]) },
         "bids": @person.membership.bids.map do |p|
           p.as_json(only: %w[id start_date end_date amount shares created_at person_id])
         end

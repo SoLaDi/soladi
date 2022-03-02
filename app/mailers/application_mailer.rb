87b01861f87c755f7408e829d604ba399ed99d85
marco-hoyer <3943275+marco-hoyer@users.noreply.github.com>
2022-03-02 23:42:52 +0100

    remove cc sending of emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["EMAIL_FROM_ADDRESS"],
-          cc: ENV["EMAIL_FROM_ADDRESS"]
+  default from: ENV["EMAIL_FROM_ADDRESS"]
   layout 'mailer'
 end
